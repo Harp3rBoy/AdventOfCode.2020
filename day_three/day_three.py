fcebb4d5414d4b5c683be8a7bba134dcf56faec4
Ben Harper <ben27.harper@gmail.com>
2020-12-03 14:00:28 +0000

    added day-three part two code

--- day_three/day_three.py
+++ day_three/day_three.py
@@ -1,3 +1,5 @@
+import math
+
 f = open("input.txt", "r")
 lines = f.read().splitlines()
 f.close()
@@ -14,4 +16,20 @@ def part1(file):
     return trees
 
 
+def part2(file):
+    slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+    trees_list = []
+    for slope in slopes:
+        trees = 0
+        x = slope[0]
+        for y in range(slope[1], len(lines) - 1, slope[1]):
+            line = lines[y] * 100
+            if line[x] == '#':
+                trees += 1
+            x += slope[0]
+        trees_list.append(trees)
+    return math.prod(trees_list)
+
+
 print(part1(lines))
+print(part2(lines))
